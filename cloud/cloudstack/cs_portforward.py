b572dcabaa6f2a96e44ea74934ccde96dfe1bf3b
René Moser <mail@renemoser.net>
2016-10-06 08:30:31 +0200

    cloudstack: cs_portforward: fix rule not found if domain is not account's domain. (#3093)
    cs_portforward will not find the rule and tries to create it resulting in an API error.
    
    Thanks to @mostkopf for reporting.

--- cloud/cloudstack/cs_portforward.py
+++ cloud/cloudstack/cs_portforward.py
@@ -272,6 +272,8 @@ class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
 
             args = {}
             args['ipaddressid'] = self.get_ip_address(key='id')
+            args['account'] = self.get_account(key='name')
+            args['domainid'] = self.get_domain(key='id')
             args['projectid'] = self.get_project(key='id')
             portforwarding_rules = self.cs.listPortForwardingRules(**args)
 
@@ -304,6 +306,8 @@ class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
         args['vmguestip']           = self.get_vm_guest_ip()
         args['ipaddressid']         = self.get_ip_address(key='id')
         args['virtualmachineid']    = self.get_vm(key='id')
+        args['account']             = self.get_account(key='name')
+        args['domainid']            = self.get_domain(key='id')
 
         portforwarding_rule = None
         self.result['changed'] = True
